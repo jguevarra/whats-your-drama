eb4c2a4dd435cbea3534b4648f6073e5810f072e
jguevarra <jfg3xs@virginia.edu>
2020-05-26 02:24:08 -0400

    finished laying out detail page

--- src/app/drama-details/drama-details.component.ts
+++ src/app/drama-details/drama-details.component.ts
@@ -3,20 +3,45 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
+// listen to changes in the router
+import { ActivatedRoute } from '@angular/router';
+
+export interface Drama {
+  actors: [];
+  desc: string;
+  directors: [];
+  finished: string;
+  genres: [];
+  imgURL: string;
+  title: string;
+  imgPoster: string;
+ }
+
 @Component({
   selector: 'app-drama-details',
   templateUrl: './drama-details.component.html',
   styleUrls: ['./drama-details.component.css']
 })
 export class DramaDetailsComponent implements OnInit {
-  public item: AngularFirestoreDocument<any[]>;
 
-  constructor(public db: AngularFirestore) 
+  public drama$: Observable<Drama>; // Observable of type Drama
+  public id: String; // will hold id passed through route (:id)
+
+  constructor(
+    public db: AngularFirestore, 
+    public route: ActivatedRoute
+  ) 
   {
-    this.item = this.db.doc('/dramas/3rkO4udPPDT9fPwvhazu');
+    // set 'id' when page loads from route params.id
+    this.route.params.subscribe(params => this.id = params.id);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    // query Firestore using 'id' when page loads
+    this.drama$ = this.db
+      .doc<Drama>('dramas/' + this.id)
+      .valueChanges();
   }
 
   
--- src/app/dramalist/dramalist.component.ts
+++ src/app/dramalist/dramalist.component.ts
@@ -3,6 +3,17 @@ import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+export interface Drama {
+  actors: [];
+  desc: string;
+  directors: [];
+  finished: string;
+  genres: [];
+  imgURL: string;
+  title: string;
+  imgPoster: string;
+ }
+
 @Component({
   selector: 'app-dramalist',
   templateUrl: './dramalist.component.html',
@@ -10,12 +21,24 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class DramalistComponent implements OnInit {
 
+  // public items: Observable<any[]>;
   public items: Observable<any[]>;
 
   constructor(public db: AngularFirestore) 
   {
-    this.items = db.collection('/dramas').valueChanges({ idField: 'dramaId' });
+    // this.items = db.collection('/dramas').valueChanges({ idField: 'dramaId' });
+    this.items = this.db.collection<Drama>('dramas')
+         .snapshotChanges().pipe(
+           map(actions => actions.map(a => {
+             const data = a.payload.doc.data() as Drama;
+             const dramaId = a.payload.doc.id;
+             return { dramaId, ...data };
+           }))
+         );
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void 
+  {
+    this.items.subscribe(data => console.log(data));
+  }
 }
